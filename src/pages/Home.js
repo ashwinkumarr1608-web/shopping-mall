b7912d5dd14c9992330bcfa2d13819079cfd7ccd
Ashwin Kumar <ashwinkumarr1608@gmail.com>
2026-06-15 19:10:20 +0530

    Removed duplicate navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,6 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/products">Products</Link>
-        <Link to="/cart">Cart</Link>
-        <Link to="/admin">Admin</Link>
-      </nav>
-
       <div className="container">
         <h1>Online Shopping Mall</h1>
         <h2>Welcome Ashwin 😎</h2>
