08b273e7327e2da26b7b24461068ca69834cd405
Ashwin Kumar <ashwinkumarr1608@gmail.com>
2026-06-16 11:33:08 +0530

    removed navi

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+
 
 function Register() {
 
@@ -8,7 +8,7 @@ function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const navigate = useNavigate();
+
 
   function handleRegister() {
 
