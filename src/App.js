11dcfd714dbb29d5ad683c8417fe2c710ecee8a7
Ashwin Kumar <ashwinkumarr1608@gmail.com>
2026-06-16 13:41:57 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
-
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
@@ -8,71 +7,53 @@ import Admin from "./pages/Admin";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import VerifyOTP from "./pages/VerifyOTP";
-
 function App() {
-
   const [cart, setCart] = useState([]);
-
   return (
-
     <BrowserRouter>
-
       <div
         style={{
           backgroundColor: "black",
           padding: "15px"
         }}
       >
-
-        <a
-          href="/home"
+        <Link
+          to="/home"
           style={{ color: "white", marginRight: "20px" }}
         >
           Home
-        </a>
-
-        <a
-          href="/products"
+        </Link>
+        <Link
+          to="/products"
           style={{ color: "white", marginRight: "20px" }}
         >
           Products
-        </a>
-
-        <a
-          href="/cart"
+        </Link>
+        <Link
+          to="/cart"
           style={{ color: "white", marginRight: "20px" }}
         >
           Cart
-        </a>
-
-        <a
-          href="/login"
+        </Link>
+        <Link
+          to="/login"
           style={{ color: "white", marginRight: "20px" }}
         >
           Login
-        </a>
-
-        <a
-          href="/register"
+        </Link>
+        <Link
+          to="/register"
           style={{ color: "white" }}
         >
           Register
-        </a>
-
+        </Link>
       </div>
-
       <Routes>
-
         <Route path="/" element={<Home />} />
-
         <Route path="/home" element={<Home />} />
-
         <Route path="/login" element={<Login />} />
-
         <Route path="/register" element={<Register />} />
-
         <Route path="/verify" element={<VerifyOTP />} />
-
         <Route
           path="/products"
           element={
@@ -82,7 +63,6 @@ function App() {
             />
           }
         />
-
         <Route
           path="/cart"
           element={
@@ -91,15 +71,9 @@ function App() {
             />
           }
         />
-
         <Route path="/admin" element={<Admin />} />
-
       </Routes>
-
     </BrowserRouter>
-
   );
-
 }
-
 export default App;
